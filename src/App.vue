a07d7fedb2188ef97ba49cb03d2958645fa3381b
Buğra Gündüz <bugragndz@gmail.com>
2017-12-17 00:21:14 +0300

    Frontend for the main page
    Added the room list and room creation button design.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,69 @@
 <template>
-	<div>
-		Working
+	<div class="container">
+		<div class="header">
+			<h1>CubeCollab</h1>
+		</div>
+		<div class="innercontainer">
+			<div class="left">
+				<div class="join">
+					<div class="roomheads threegrid">
+						<span class="name">Room Name</span>
+						<span class="users">Users</span>
+						<span class="event">Event</span>
+					</div>
+					<ul class="rooms">
+						<li class="threegrid">
+							<span class="name">Room1</span>
+							<span class="users">42</span>
+							<span class="event">Magic</span>
+						</li>
+						<li class="threegrid">
+							<span class="name">Room2</span>
+							<span class="users">42</span>
+							<span class="event">Magic</span>
+						</li>
+						<li class="threegrid">
+							<span class="name">Room3</span>
+							<span class="users">42</span>
+							<span class="event">Magic</span>
+						</li>
+						<li class="threegrid">
+							<span class="name">Room4</span>
+							<span class="users">42</span>
+							<span class="event">Magic</span>
+						</li>
+						<li class="threegrid">
+							<span class="name">Room1</span>
+							<span class="users">42</span>
+							<span class="event">Magic</span>
+						</li>
+						<li class="threegrid">
+							<span class="name">Room2</span>
+							<span class="users">42</span>
+							<span class="event">Magic</span>
+						</li>
+						<li class="threegrid">
+							<span class="name">Room3</span>
+							<span class="users">42</span>
+							<span class="event">Magic</span>
+						</li>
+						<li class="threegrid">
+							<span class="name">Room4</span>
+							<span class="users">42</span>
+							<span class="event">Magic</span>
+						</li>
+					</ul>
+				</div>
+				<div class="create">
+					Create a Room
+				</div>
+			</div>
+			<div class="right">
+				<div class="login">
+					
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
